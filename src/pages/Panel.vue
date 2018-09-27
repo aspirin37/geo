65d69598d570f8faf4bcee16c83be7ff798ae586
Maksim <pelmesh10@gmail.com>
2018-09-27 19:34:06 +0300

    styles init

---
+++ src/pages/Panel.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button class="btn btn-outline-theme">hello</button>
+        <!-- <navbar />
+        <main class="main-content flex-column flex-md-row d-flex align-items-start align-items-md-stretch flex-wrap flex-md-nowrap"
+              :class="{'main-content--small' : smallSize, 'chrome-mobile': isMobileChrome}">
+            <sidebar v-on:toggleSize="toggleSidebar" />
+            <div class="w-100 overflow-hidden">
+                <transition name="page"
+                            mode="out-in">
+                    <keep-alive include="Users">
+                        <router-view />
+                    </keep-alive>
+                </transition>
+            </div>
+        </main> -->
+    </div>
+</template>
+<script>
+// import Navbar from '../components/Navbar'
+// import Sidebar from '../components/Sidebar'
+
+export default {
+    name: 'panel',
+    components: {
+        // Navbar,
+        // Sidebar
+    },
+    data() {
+        return {}
+    },
+    methods: {
+
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Panel from '@/pages/Panel'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'Panel',
+        component: Panel,
+        // children: [{
+        //     path: '',
+        //     name: 'Main',
+        //     component: Main
+        // }]
+    }]
 })
