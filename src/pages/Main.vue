6f482748e888eedff87f81e52d9e5a906e4aeb7f
Maksim <pelmesh10@gmail.com>
2018-10-04 19:33:27 +0300

    banner

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -10,6 +10,8 @@
         </div>
         <div class="page-container">
             <news></news>
+            <img class="main-page-banner"
+                 src="https://s.go31.ru/section/newsInText/upload/images/news/intext/000/040/533/to-1_5b72aa1f88e4f.jpg">
         </div>
     </div>
 </template>
@@ -34,4 +36,8 @@ export default {
 </script>
 <style lang="scss"
        scoped>
+.main-page-banner {
+    width: 100%;
+    height: auto;
+}
 </style>
