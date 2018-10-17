94eec107e425e0bd9397d98c5b2cd32d494f54d9
Maksim <pelmesh10@gmail.com>
2018-10-17 18:58:31 +0300

    radial timer

--- src/components/main/TopSlider.vue
+++ src/components/main/TopSlider.vue
@@ -1,5 +1,6 @@
 <template>
     <section class="main-slider">
+        <!-- <div id="container"></div> -->
         <!-- <img src="../../assets/geo-bg.jpg"> -->
         <swiper class="slider"
                 :options="swiperOption">
@@ -7,10 +8,17 @@
                           :key="i">
                 <img src="../../assets/geo-bg.jpg">
             </swiper-slide>
+            <div class="swiper-pagination swiper-pagination-white"
+                 slot="pagination"></div>
+            <div class="swiper-button-prev swiper-button-white"
+                 slot="button-prev"></div>
+            <div class="swiper-button-next swiper-button-white"
+                 slot="button-next"></div>
         </swiper>
     </section>
 </template>
 <script>
+var ProgressBar = require('progressbar.js');
 export default {
     data() {
         return {
@@ -26,14 +34,49 @@ export default {
             swiperOption: {
                 loop: true,
                 navigation: {
-                    prevEl: '.people-prev',
-                    nextEl: '.people-next',
+                    prevEl: '.swiper-button-prev',
+                    nextEl: '.swiper-button-next',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    bulletClass: 'top-slider-bullet',
+                    bulletActiveClass: 'top-slider-bullet-active'
                 },
                 autoplay: {
                     delay: 10000,
                     disableOnInteraction: false
                 },
                 speed: 1000,
+                on: {
+                    slideChange: () => {
+                        this.drawCircle(false)
+                    },
+                },
+            }
+        }
+    },
+    mounted() {
+        this.drawCircle(true)
+    },
+    methods: {
+        drawCircle(init) {
+            let bullets = document.querySelectorAll('.top-slider-bullet')
+            bullets.forEach(it => {
+                if (it.firstChild) it.removeChild(it.firstChild)
+            })
+            if (document.querySelector('.top-slider-bullet-active')) {
+                const bar = new ProgressBar.Circle('.top-slider-bullet-active', {
+                    strokeWidth: 7,
+                    easing: 'linear',
+                    duration: 10500,
+                    color: 'white',
+                    trailColor: 'transparent',
+                    trailWidth: 0,
+                    svgStyle: null,
+                });
+                const container = bar._container
+
+                bar.animate(1.0);
             }
         }
     }
@@ -42,6 +85,18 @@ export default {
 <style lang="scss"
        scoped>
 @import '@/styles/_variables.scss';
+
+.swiper-button-next,
+.swiper-button-prev {
+    height: 32px;
+    opacity: 0.5;
+    transition: 0.3s;
+    background-size: contain;
+
+    &:hover {
+        opacity: 1;
+    }
+}
 
 .main-slider {
     width: 100%;
@@ -60,5 +115,45 @@ export default {
         min-width: auto;
         padding: 0;
     }
+}
+</style>
+<style lang="scss">
+.top-slider-bullet {
+    display: inline-block;
+    position: relative;
+    width: 20px;
+    height: 20px;
+
+    &::after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 8px;
+        height: 8px;
+        transform: translate(-50%, -50%);
+        display: block;
+        border-radius: 100%;
+        background: #000;
+        opacity: 0.2;
+    }
+
+    &-active::after {
+        opacity: 1;
+        background-color: white;
+    }
+
+    // width: 8px;
+    // height: 8px;
+    // margin: 0 8px;
+    // display: inline-block;
+    // border-radius: 100%;
+    // background: #000;
+    // opacity: 0.2;
+
+    // &-active {
+    //     opacity: 1;
+    //     background-color: white;
+    // }
 }
 </style>
