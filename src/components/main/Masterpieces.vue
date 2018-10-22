6a625174c6f199d0450194f7a616720644564259
Maksim <pelmesh10@gmail.com>
2018-10-22 15:05:46 +0300

    masterpieces

--- src/components/main/Masterpieces.vue
+++ src/components/main/Masterpieces.vue
@@ -66,16 +66,14 @@ export default {
     computed: {
         swiperOptionTop() {
             return {
-                // effect: this.$mq !== 'lg' ? 'fade' : '',
+                effect: this.$mq !== 'lg' ? 'fade' : '',
                 navigation: {
                     nextEl: '.master-next',
                     prevEl: '.master-prev',
                     disabledClass: 'control-btn--disabled'
                 },
-                loop: true,
-                loopedSlides: this.masterpieces.length,
-                // loop: this.$mq === 'lg',
-                // loopedSlides: this.$mq === 'lg' ? this.masterpieces.length : false,
+                loop: this.$mq === 'lg',
+                loopedSlides: this.$mq === 'lg' ? this.masterpieces.length : false,
                 speed: 700,
             }
         },
@@ -85,8 +83,7 @@ export default {
                 slidesPerView: 'auto',
                 touchRatio: 0.2,
                 slideToClickedSlide: true,
-                // loop: this.$mq === 'lg',
-                loop: true,
+                loop: this.$mq === 'lg',
                 loopedSlides: this.masterpieces.length,
                 slidesPerView: 6,
                 spaceBetween: 8,
