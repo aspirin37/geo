7a06282dc3f80bda59dabaa321d10597e2579f4b
Maksim <pelmesh10@gmail.com>
2018-10-22 17:49:25 +0300

    menu

--- src/components/layout/PageHeader.vue
+++ src/components/layout/PageHeader.vue
@@ -89,7 +89,7 @@ export default {
     border-bottom: 2px solid black;
 
     @media (max-width: $desktop-width) {
-        position: fixed;
+        position: absolute;
         top: 64px;
         left: 0;
         z-index: 100;
