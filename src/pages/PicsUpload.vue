660bd10d029c6242ae514d9dbb3d06bc6960bc3c
Maksim <pelmesh10@gmail.com>
2018-11-01 19:22:59 +0300

    dropzone init

--- src/pages/PicsUpload.vue
+++ src/pages/PicsUpload.vue
@@ -1,57 +1,62 @@
 <template>
     <main class="page-container">
-        <dropzone ref="dropzone"
+        <!-- <dropzone ref="dropzone"
                   id="dropzone"
                   :options="dropzoneOptions"
                   @vdropzone-file-added="addFile"></dropzone>
         <div class="dropzone dropzone-gallery">
-        </div>
-        <draggable v-model="files">
-            <img class="test"
-                 v-for="(it, i) in files"
-                 :src="it.file"
-                 :key="i">
+        </div> -->
+        <drop class="dropzone"
+              @drop="handleDrop">
+            Drop something here
+        </drop>
+        <draggable class="dropzone-gallery"
+                   :options="{animation: 300}"
+                   v-model="files">
+            <img v-for="(it, i) in files"
+                 :key="i"
+                 :src="it.src">
         </draggable>
     </main>
 </template>
 <script>
-import Dropzone from 'vue2-dropzone'
 import Draggable from 'vuedraggable'
+import { Drop } from 'vue-drag-drop';
 
 export default {
     components: {
-        Dropzone,
-        Draggable
-    },
-    mounted() {
-        this.$nextTick(() => {
-            document.querySelector('#dropzone').classList.remove('vue-dropzone')
-        })
-
+        Draggable,
+        Drop
     },
     data() {
         return {
-            dropzoneOptions: {
-                url: '/',
-                autoProcessQueue: false,
-                thumbnailWidth: 120,
-                thumbnailHeight: 120,
-                previewsContainer: '.dropzone-gallery',
-            },
             files: [],
             filesLength: 0,
         }
     },
     methods: {
         addFile(file) {
-            let obj = {
-                file,
-                name: file.name,
-                fixed: false,
-                order: this.filesLength
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = () => {
+                let obj = {
+                    file,
+                    src: reader.result,
+                    name: file.name,
+                    fixed: false,
+                    order: this.filesLength
+                }
+                this.files.push(obj)
+                this.filesLength++
             }
-            this.files.push(obj)
-            this.filesLength++
+
+        },
+        handleDrop(data, evt) {
+            event.preventDefault();
+            const files = event.dataTransfer.files;
+            for (let i = 0; i < files.length; i++) {
+                this.addFile(files.item(i))
+            }
         }
     }
 }
@@ -68,22 +73,33 @@ export default {
 <style lang="scss">
 @import '@/styles/_variables.scss';
 
-.dz-progress {
-    opacity: 0 !important;
+// .dz-progress {
+//     opacity: 0 !important;
+// }
+
+// .dropzone .dz-preview:hover .dz-details {
+//     opacity: 0 !important;
+// }
+
+// .dropzone .dz-preview:hover .dz-image img {
+//     filter: none;
+//     transform: none;
+// }
+
+.dropzone-gallery {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
 }
 
-.dropzone .dz-preview:hover .dz-details {
-    opacity: 0 !important;
-}
-
-.dropzone .dz-preview:hover .dz-image img {
-    filter: none;
-    transform: none;
-}
-
-.test {
+.dropzone-gallery img {
+    margin: 10px;
+    border-radius: 20px;
+    overflow: hidden;
     width: 120px;
     height: 120px;
-    background: #eee;
+    position: relative;
+    display: block;
+    z-index: 10;
 }
 </style>
